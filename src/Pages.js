8188728f0c65deaf8f069884f0aa56af1578899c
windows <windows@microsoft.com>
2023-01-03 14:29:03 +0530

    change

---
+++ src/Pages.js
@@ -0,0 +1,25 @@
+import React, { Fragment, useState } from "react";
+import './style.css';
+
+export const Blockbox = ()=>{
+
+const [statevariable, statefunction]=useState(10);
+
+// const abc= ()=>{
+//     statefunction(60)
+// }
+
+const abc= (inputvalue)=>{
+    statefunction(inputvalue.target.value);
+    console.log(inputvalue);
+}
+
+    return(
+        <Fragment>
+        <h1 className="one">this is box</h1>
+            <input type="text" placeholder="enter number" value={statevariable} style={{padding:10}} onInput={abc} />
+            <h1>{statevariable}</h1>
+        </Fragment>
+    )
+
+}
