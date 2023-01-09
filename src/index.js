b4984fa9ca346781712b1e34d576c2afcc4e5d53
windows <windows@microsoft.com>
2023-01-09 14:33:45 +0530

    use core ui

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Myhome from './Home';
+import { Myhome, Mynavbar } from './components/Welcome';
+import Myhome1 from './components/Welcome';
+import './components/global.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Myhome></Myhome>
-    
+    <Mynavbar/>
+   <Myhome/>
+   <Myhome1/>
   </React.StrictMode>
 );
 
